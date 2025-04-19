498ac059918880087c66a80afd904d1ccd6de085
Huỳnh Đức Nhân <nhan.huynhgl272@hcmut.edu.vn>
2025-04-19 16:57:00 +0700

    sys_kill_all

--- ossim_sierra/src/sys_killall.c
+++ ossim_sierra/src/sys_killall.c
@@ -15,6 +15,62 @@
 #include "queue.h" //????????????????
 #include "stdlib.h" //????????????????
 
+int check_name(char *proc_name, char *path)
+{
+    int name_index = 0, path_index = 0;
+    int same_name = 1;
+
+    while (path_index < 100 && path[path_index] != '\0') ++path_index; // Find the end of the string
+    while (path_index >= 0 && path[path_index] != '/') --path_index; // Find the last '/'
+    ++path_index; // Move to the start of the process name
+
+    while (proc_name[name_index] != '\0' && path[path_index] != '\0') {
+        if (proc_name[name_index] != path[path_index]) {
+            same_name = 0;
+            break;
+        }
+        ++name_index;
+        ++path_index;
+    }
+    if (proc_name[name_index] != '\0' || path[path_index] != '\0') {
+        same_name = 0;
+    }
+
+    return same_name;
+}
+
+int kill_in_queue(struct queue_t *proc_queue, char *proc_name)
+{
+    struct queue_t new_queue;
+    new_queue.size = 0;
+    int count = 0;
+    
+    for (int i = 0; i < proc_queue->size; ++i) {
+        struct pcb_t *proc = proc_queue->proc[i];
+        if (proc == NULL) continue;
+
+        int same_name = check_name(proc_name, proc->path);
+
+        if (same_name == 1) {
+            printf("Process %s killed\n", proc_name);
+            // free ?????????
+            free(proc);
+            ++count;
+        }
+        else {
+            new_queue.proc[new_queue.size] = proc;
+            new_queue.size++;
+        }
+    }
+
+    proc_queue->size = new_queue.size;
+    for (int i = 0; i < new_queue.size; ++i) {
+        proc_queue->proc[i] = new_queue.proc[i];
+    }
+
+    return count;
+}
+
 int __sys_killall(struct pcb_t *caller, struct sc_regs* regs)
 {
     char proc_name[100];
@@ -41,98 +97,50 @@ int __sys_killall(struct pcb_t *caller, struct sc_regs* regs)
      */
     //caller->running_list
     //caller->mlq_ready_queue    
+    int killed_count = 0; // Expected output
 
+    struct queue_t *running_list = caller->running_list;
+    if (running_list != NULL) {
+        killed_count += kill_in_queue(running_list, proc_name);
+    }
 
     #ifdef MLQ_SCHED
-        for (int mlq_index = 0; mlq_index < MAX_PRIO; ++mlq_index) {
-            struct queue_t *ready_queue = &caller->mlq_ready_queue[mlq_index];
-            int queue_index = 0;
-            while (queue_index < ready_queue->size) {
-                struct pcb_t *proc = ready_queue->proc[queue_index];
-                int name_index = 0, path_index = 0;
-                int same_name = 1;
-
-                while (path_index < 100 && proc->path[path_index] != '\0') ++path_index; // Find the end of the string
-                while (path_index >= 0 && proc->path[path_index] != '/') --path_index; // Find the last '/'
-                ++path_index; // Move to the start of the process name
-
-                while (proc_name[name_index] != '\0' && proc->path[path_index] != '\0') {
-                    if (proc_name[name_index] != proc->path[path_index]) {
-                        same_name = 0;
-                        break;
-                    }
-                    ++name_index;
-                    ++path_index;
+        struct queue_t *mlq_ready_queue = caller->mlq_ready_queue;
+        if (mlq_ready_queue != NULL) {
+            for (int i = 0; i < mlq_ready_queue->size; ++i) {
+                struct queue_t *queue = &(mlq_ready_queue[i]);
+                if (queue != NULL) {
+                    killed_count += kill_in_queue(queue, proc_name);
                 }
-                if (proc_name[name_index] != '\0' || proc->path[path_index] != '\0') {
-                    same_name = 0;
-                }
-
-                if (same_name == 1) {
-                    // remove_proc(ready_queue, queue_index);
-                    if (proc->page_table != NULL) free(proc->page_table);
-                    if (proc->code != NULL) {
-                        if (proc->code->text != NULL) free(proc->code->text);
-                        free(proc->code);
-                    }
-                    #ifdef MM_PAGING
-                        if (proc->mm != NULL) {
-                            // Delete the paging ??????????????
-                            free(proc->mm);
-                        }
-                    #endif
-                    free(proc);
-                }
-                else ++queue_index;
             }
         }
     #else
-        int queue_index = 0;
-        struct queue_t *running_list = caller->running_list;
-        while (queue_index < running_list->size) {
-            struct pcb_t *proc = running_list->proc[queue_index];
-            int name_index = 0, path_index = 0;
-            bool same_name = true;
-
-            while (path_index < 100 && proc->path[path_index] != '\0') ++path_index; // Find the end of the string
-            while (path_index >= 0 && proc->path[path_index] != '/') --path_index; // Find the last '/'
-            ++path_index; // Move to the start of the process name
-
-            while (proc_name[name_index] != '\0' && proc->path[path_index] != '\0') {
-                if (proc_name[name_index] != proc->path[path_index]) {
-                    same_name = false;
-                    break;
-                }
-                ++name_index;
-                ++path_index;
-            }
-            if (proc_name[name_index] != '\0' || proc->path[path_index] != '\0') {
-                same_name = false;
-            }
-
-            if (same_name) {
-                remove_proc(running_list, queue_index);
-                if (proc->page_table != NULL) free(proc->page_table);
-                if (proc->code != NULL) {
-                    if (proc->code->text != NULL) free(proc->code->text);
-                    free(proc->code);
-                }
-                #ifdef MM_PAGING
-                    if (proc->mm != NULL) {
-                        // Delete the paging ??????????????
-                        free(proc->mm);
-                    }
-                #endif
-                free(proc);
-            }
-            else ++queue_index;
+        struct queue_t *ready_queue = caller->ready_queue;
+        if (ready_queue != NULL) {
+            killed_count += kill_in_queue(ready_queue, proc_name);
         }
     #endif
+
 
     /* TODO Maching and terminating 
      *       all processes with given
      *        name in var proc_name
      */
 
-    return 0; 
+    // if (same_name) {
+    //     remove_proc(running_list, queue_index);
+    //     if (proc->page_table != NULL) free(proc->page_table);
+    //     if (proc->code != NULL) {
+    //         if (proc->code->text != NULL) free(proc->code->text);
+    //         free(proc->code);
+    //     }
+    //     #ifdef MM_PAGING
+    //         if (proc->mm != NULL) {
+    //             // Delete the paging ??????????????
+    //             free(proc->mm);
+    //         }
+    //     #endif
+    //     free(proc);
+    // }
+    return killed_count; // Expected output
 }
